1efe41e1c66128544c06e90b6b1a67d8dbf29af4
damilolajohn6 <damilolajohn622@gmail.com>
2023-11-02 15:52:06 +0000

    encrypt password

--- encrypt_password.py
+++ encrypt_password.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python3
 """
-	encrypt passwor
+Defines a hash_password function to return a hashed password
 """
-
 import bcrypt
+from bcrypt import hashpw
 
 
 def hash_password(password: str) -> bytes:
-    """ Salted pass generation
     """
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    Returns a hashed password
+    Args:
+        password (str): password to be hashed
+    """
+    b = password.encode()
+    hashed = hashpw(b, bcrypt.gensalt())
+    return hashed
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ is valid?
     """
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    Check whether a password is valid
+    Args:
+        hashed_password (bytes): hashed password
+        password (str): password in string
+    Return:
+        bool
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
